f29012c3b23e6ff7099b3bff6f530d87734cd17a
Vinicíus Andrade <vinicius.andrade@dce.ufpb.br>
2013-12-26 21:00:14 -0200

    ..

--- Teste.py
+++ Teste.py
@@ -4,9 +4,9 @@ from controles.GerenteGitHub import GerenteGitHub
 gg = GerenteGitHub()
 
 # usuario = raw_input('digite um nome de usuario: ')
-#gg.salvarRepos("rabble")
+gg.salvarRepos("brixen")
 
-gg.salvarReposFaltando("rabble", "repos")
+#gg.salvarReposFaltando("jsierles", "driven")
 
 # dicionarioLinguagens = gg.getLinguagensNoArquivo("mojombo")
 # keys = dicionarioLinguagens.keys()
